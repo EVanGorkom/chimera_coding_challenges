b819e05baa34bab8b5e2e1d039d093190bdc4c8c
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-14 15:41:45 -0600

    Refactor some of the redundant code

--- ruby/palindrome_next.rb
+++ ruby/palindrome_next.rb
@@ -18,34 +18,32 @@
 def find_next_palindrome(num)
   string_num = num.to_s
   length = string_num.length
+  copy_length = length / 2
 
   return 11 if length == 1 || num == 10
   
   if length.even?
-    copy_length = length / 2
     first_half = string_num.slice(0, copy_length)
     second_half = string_num.slice(copy_length..-1)
     if first_half.to_i < second_half.to_i
       new_num_half = first_half.to_i + 1
       reverse = new_num_half.to_s.reverse
       final = new_num_half.to_s + reverse
-    elsif first_half.to_i > second_half.to_i
+    else 
       final = first_half + first_half.reverse
     end
-    final.to_i
-  else length.odd?
+  else
     mid_point = string_num[(length / 2)]
     string_num.delete(mid_point)
-    copy_length = length / 2
     first_half = string_num.slice(0, copy_length)
     second_half = string_num.slice((length / 2)..-1)
     if first_half.to_i < second_half.to_i
       final = first_half + (mid_point.to_i + 1).to_s + first_half.reverse
-    elsif first_half.to_i > second_half.to_i
+    else
       final = first_half + mid_point + first_half.reverse
     end
-    final.to_i
   end
+  final.to_i
 end
 
 p find_next_palindrome(12134)
@@ -53,3 +51,4 @@ p find_next_palindrome(123)
 p find_next_palindrome(9876)
 p find_next_palindrome(10023)
 p find_next_palindrome(12)
+p find_next_palindrome(101)
