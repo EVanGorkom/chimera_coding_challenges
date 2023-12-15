a6351bbea25eebedab6b3218eacb2491a4c6ff1c
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-14 21:54:59 -0600

    Solve the highest_lowest challenge in ruby, and then refactored with a simplier solution

--- ruby/highest_lowest.rb
+++ ruby/highest_lowest.rb
@@ -11,6 +11,28 @@
 # Output string must be two numbers separated by a single space, and highest number is first.
 
 
-def high_and_low(numbers):
-  return numbers
+def high_and_low(numbers)
+  array = numbers.split
+  nums = array.map do |num|
+    num.to_i
+  end
+  
+  sorted = nums.sort
+
+  ## first attempt
+  # high = nums[0]
+  # low = nums[0]
+  # nums.each do |num|
+  #   if num > high
+  #     high = num
+  #   elsif num < low
+  #     low = num
+  #   end
+  # end
+  
+  return "#{sorted.last} #{sorted.first}"
 end
+
+p high_and_low("-2 -1 0 1 2 3 4 5")
+p high_and_low("-5 -4 -3 -2 -1 0 1 2 3 4 5")
+p high_and_low("0 1 2 3 4 5 6 7 8 9 10")
