cfc01d58c4f128ae3a81a2821d529c74ad5417a0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-18 22:18:32 -0600

    Finish the towerBuilder challenge in ruby and add a simplier solution for reflection

--- ruby/tower.rb
+++ ruby/tower.rb
@@ -20,5 +20,36 @@
 # ]
 
 def towerBuilder(n_floors)
-  return []
+  tower_array = []
+  counter = 1
+
+  n_floors.times do 
+    floor_string = ""
+    counter.times do
+      floor_string << "*"
+    end
+    tower_array << floor_string
+    counter += 2
+  end
+
+  new_array = tower_array.map do |floor|
+    diff = tower_array[-1].length - floor.length
+    (diff / 2).times do
+      floor = " " + floor + " "
+    end
+    floor
+  end
 end
+
+p towerBuilder(3)
+p towerBuilder(5)
+p towerBuilder(6)
+
+# # Simplified answer:
+# def towerBuilder(n)
+#   (1..n).map do |i|
+#     space = ' ' * (n - i)
+#     stars = '*' * (i * 2 - 1)
+#     space + stars + space
+#   end
+# end
