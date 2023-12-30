85f50c4e01c093c8429f1b744a764c7e9e33e110
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-30 11:34:20 -0600

    Finish the vowel count challenge in python

--- python/vowel_count.py
+++ python/vowel_count.py
@@ -2,5 +2,24 @@
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 # The input string will only consist of lower case letters and/or spaces.
 
+## Initial solution (no refactor) 
+# def get_count(sentence):
+#   count = 0
+#   count += sentence.count("a")
+#   count += sentence.count("e")
+#   count += sentence.count("i")
+#   count += sentence.count("o")
+#   count += sentence.count("u")
+#   return count
+
+## Refactored solution
 def get_count(sentence):
-  pass
+  vowels = "aeiouAEIOU"
+  count = sum(sentence.count(vowel) for vowel in vowels)
+  return count
+
+  # here we're looking at each char in the sentence.
+  #   is char == vowel from vowels string?
+  #     add to the count if it is!
+
+print(get_count("This is a long sentence with a lot of vowels for counting."))
