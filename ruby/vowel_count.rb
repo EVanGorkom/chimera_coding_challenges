322d64639be6474d69914c9b5b19400bd352dfab
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-30 11:21:01 -0600

    Finish the vowel_count challenge in ruby

--- ruby/vowel_count.rb
+++ ruby/vowel_count.rb
@@ -2,20 +2,19 @@
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 # The input string will only consist of lower case letters and/or spaces.
 
-# vowels = {'a' => 0, 'e' => 0, 'i' => 0, 'o' => 0, 'u' => 0} #This is more complicated than the problem was asking for.
-
 def get_count(input_str)
-  str_array = input_str.downcase.split
+  string = input_str.downcase
   vowels = ['a', 'e', 'i', 'o', 'u']
-  vowel_count = 0
-  str_array.each do |word|
-    vowels.each do |vowel|
-      if word.include?(vowel)
-        vowel_count += 1
-      end
-    end
+  vowel_count = []
+  string.each_char do |char|
+    vowel_count << char if vowels.include?(char)
   end
-  p vowel_count
+  p vowel_count.length
+end
+
+# Alternative / Simplified Solution
+def get_count(inputStr)
+  p inputStr.downcase.count("aeiou")
 end
 
 get_count("This is a sentence with a lot vowels for counting.")
