2357f76049f039369c7aafb84305f71abcc77c27
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-16 22:11:44 -0600

    Finish the readable_time challenge in python

--- python/readable_time.py
+++ python/readable_time.py
@@ -6,6 +6,39 @@
 # The maximum time never exceeds 359999 (99:59:59)
 
 # You can find some examples in the test fixtures.
+import pdb
 
-def make_readable(seconds):
-  pass
+def make_readable(input):
+  if input > 356399:
+    final = "99:59:59"
+  else:
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    while input > 59:
+      input -= 60
+      minutes += 1
+      if minutes > 59:
+        minutes = 0
+        hours += 1
+
+    seconds = input
+
+    if len(str(seconds)) == 1:
+      seconds = '0' + str(seconds)
+
+    if len(str(minutes)) == 1:
+      minutes = '0' + str(minutes)
+
+    if len(str(hours)) == 1:
+      hours = '0' + str(hours)
+
+    final = f"{hours}:{minutes}:{seconds}"
+  return final
+
+
+print(make_readable(0)) # Should return '00:00:00'
+print(make_readable(15)) # Should return '00:00:15'
+print(make_readable(111000)) # Should return '30:50:00'
+print(make_readable(356400)) # Should return max value
