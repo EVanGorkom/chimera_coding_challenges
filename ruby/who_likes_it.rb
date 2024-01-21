755a22977a53c1c8d1b4f0cef88628958407b14b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-20 22:25:31 -0600

    Solve the who_likes_it.rb challenge

--- ruby/who_likes_it.rb
+++ ruby/who_likes_it.rb
@@ -10,5 +10,37 @@
 
 
 def likes(names)
-  #your code here
+  if names.length == 0
+    "no one likes this"
+  elsif names.length == 1
+    "#{names[0]} likes this"
+  elsif names.length == 2
+    "#{names[0]} and #{names[1]} like this"
+  elsif names.length == 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  elsif names.length >= 4
+    "#{names[0]}, #{names[1]} and #{(names.length - 2)} others like this"
+  end
 end
+
+## Alternate solution using 'when' cases
+# def likes(names)
+#   case names.size
+#   when 0 
+#     "no one likes this"
+#   when 1 
+#     "#{names[0]} likes this"
+#   when 2
+#     "#{names[0]} and #{names[1]} like this"
+#   when 3
+#     "#{names[0]}, #{names[1]} and #{names[2]} like this"
+#   else
+#     "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+#   end
+# end
+
+p likes([])
+p likes(["Peter"])
+p likes(["Jacob", "Alex"])
+p likes(["Max", "John", "Mark"])
+p likes(["Alex", "Jacob", "Mark", "Max"])
