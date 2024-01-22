48c2709064c481a38339b2912a35718c0f1d526d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-21 23:14:38 -0600

    Refactor the solution

--- ruby/parity_outlier.rb
+++ ruby/parity_outlier.rb
@@ -4,31 +4,43 @@
 # [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
 # [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
 
+# def find_outlier(integers)
+#   odd_nums = 0
+#   even_nums = 0
+
+#   integers[0..2].each do |num|
+#     if num.odd?
+#       odd_nums += 1
+#     elsif num.even?
+#       even_nums += 1
+#     end
+#   end
+
+#   if odd_nums > even_nums
+#     integers.each do |num|
+#       if num.even?
+#         return num
+#       end
+#     end
+
+#   else 
+#     integers.each do |num|
+#       if num.odd?
+#         return num
+#       end
+#     end
+#   end
+# end
+
+## Refactored//Simplified solution
 def find_outlier(integers)
-  odd_nums = 0
-  even_nums = 0
+  even_count = integers.count(&:even?)
+  odd_count = integers.count(&:odd?)
 
-  # while odd_nums < 2 || even_nums < 2
-  integers.each do |num|
-    if num.odd?
-      odd_nums += 1
-    elsif num.even?
-      even_nums += 1
-    end
-  end
-
-  if odd_nums > even_nums
-    integers.each do |num|
-      if num.even?
-        return num
-      end
-    end
+  if even_count > odd_count 
+    integers.find(&:odd?)
   else 
-    integers.each do |num|
-      if num.odd?
-        return num
-      end
-    end
+    integers.find(&:even?)
   end
 end
 
