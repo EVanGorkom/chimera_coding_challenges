2a3871f297cd58882db2af501086a3ec897bd0db
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-25 00:21:09 -0600

    Finish the first iteration of the rot13 challenge

--- javascript/rot13.js
+++ javascript/rot13.js
@@ -3,7 +3,26 @@
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
 function rot13(message){
-  
+  let final = '';
+
+  for (i = 0; i < message.length; i++) {
+    let charCode = message.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      charCode += 13
+      if (charCode > 90) {
+        charCode = (65 + (charCode - 90))
+      };
+    }
+    else if (charCode >= 97 && charCode <= 122) {
+      charCode += 13
+      if (charCode > 122) {
+        charCode = (97 + (charCode - 122))
+      };
+    };
+    final += String.fromCharCode(charCode);
+  };
+  return final;
 }
 
-console.log(rot13("Naruto"))
+console.log(rot13("NARTUO")) // 65 - 90
+console.log(rot13("naruto")) // 97 - 122
