0b7f49b6bb3a9f573c0277315af564d1d36c06d1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-04 23:23:18 -0600

    Finish the create phone number challenge and add an alternative solution

--- javascript/createPhoneNumber.js
+++ javascript/createPhoneNumber.js
@@ -6,6 +6,19 @@
 
 // Don't forget the space after the closing parentheses!
 
-function createPhoneNumber(numbers){
-  
+function createPhoneNumber(input){
+  return `(${input[0]}${input[1]}${input[2]}) ${input[3]}${input[4]}${input[5]}-${input[6]}${input[7]}${input[8]}${input[9]}`
 }
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+// // Simplified / Alternative Solution:
+// function createPhoneNumber(numbers){
+//   var format = "(xxx) xxx-xxxx";
+  
+//   for(var i = 0; i < numbers.length; i++) {
+//     format = format.replace('x', numbers[i]);
+//   }
+
+//   return format;
+// }
