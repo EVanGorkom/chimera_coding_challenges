23514b2e92b5ccefe4f672094ab3df86c97c1973
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-06 19:26:49 -0600

    Finish the booleanToString php challenge

---
+++ php/booleanToString/index.php
@@ -0,0 +1,12 @@
+<!-- Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. -->
+
+<?php
+
+function booleanToString($bool) {
+  return $bool ? 'true' : 'false';
+}
+
+echo booleanToString(true);
+?>
