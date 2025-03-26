85f79ee7fb34029d83f4a4761f0fa7fda7616172
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-03-25 21:05:59 -0500

    Finish the greatest_common_divisor challenge

--- python/leetcode75/greatest_common_divisor.py
+++ python/leetcode75/greatest_common_divisor.py
@@ -19,11 +19,19 @@ Constraints:
 str1 and str2 consist of English uppercase letters.
 """
 
+import math
+
 # Initial thoughts
+# Finding the strings that match, if any, will be the easy part. I think finding the longest string that matches will be the trick here.
+# I'll want to first check if the strings match anywhere (example 3), then for how long (example 1), then see if it can be reduced at all (example 2).
 
 
 def gcdOfStrings(str1: str, str2: str) -> str:
-    return
+    if str1 + str2 != str2 + str1:
+        return ""
+    
+    gcd_length = math.gcd(len(str1), len(str2))
+    return str1[:gcd_length]
 
 
 str1 = "ABCABC"
@@ -44,4 +52,7 @@ print(gcdOfStrings(str1, str2))
 
 # --------------------------
 # WHAT I LEARNED:
-
+# This one was pretty tricky. I began with iterating through both strings to find commonality, but my solution failed at reducing the redundant characters.
+# I had to do some research on this one to learn that the GCD MUST be a repeating pattern of at least one of the strings. For example I was initially under the impression that and example like `str1 = abcdef` and `str2 = abc` would still return the GCD of "abc", but that isn't how the challenge works.
+# With this realization I was able to simplify my code and found the `math.gcd()` function which I did not know existed previously. 
+# Finally I took the range of the string that was the GCD's length.
