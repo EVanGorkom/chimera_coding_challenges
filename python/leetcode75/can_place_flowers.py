a66f7c82adf9f61b74478cf5b0333f6e4c1d2a41
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-04-02 22:29:47 -0500

    Finish the can_place_flowers challenge

--- python/leetcode75/can_place_flowers.py
+++ python/leetcode75/can_place_flowers.py
@@ -18,20 +18,33 @@ There are no two adjacent flowers in flowerbed.
 """
 
 # Initial Thoughts
+# I'll need to likely utilize a two-pointer method to find out the previous values with the current or next values
 
 
 
 def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
-    return
+    for i in range(len(flowerbed)):
+        if (flowerbed[i] == 0 
+            and (i == 0 or flowerbed[i - 1] == 0) 
+            and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+            
+            flowerbed[i] = 1
+            n -= 1
+            
+            if n == 0:
+                return True
 
+    return n == 0
 
 
 flowerbed = [1,0,0,0,1] 
 n = 1
+print(canPlaceFlowers(flowerbed, n))
 # Answer should be: true
 
 flowerbed = [1,0,0,0,1] 
 n = 2
+print(canPlaceFlowers(flowerbed, n))
 # Answer should be: false
 
 
