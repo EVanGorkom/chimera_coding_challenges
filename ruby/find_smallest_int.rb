09e5920132655fba0258a05ca24d61346aad010d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-05-11 09:10:33 -0500

    Solve the problem again, but without the use of built-in functions

--- ruby/find_smallest_int.rb
+++ ruby/find_smallest_int.rb
@@ -6,7 +6,18 @@
 # You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
 def find_smallest_int(arr)
-  arr.min()
+  # arr.min()
+  # This is a little too easy with the built-in functions so here's the answer without it.
+
+  smallest_num = arr[0]
+
+  for num in arr
+    if num < smallest_num
+      smallest_num = num
+    end
+  end
+
+  return smallest_num
 end
 
 array = [34, 15, 88, 2]
