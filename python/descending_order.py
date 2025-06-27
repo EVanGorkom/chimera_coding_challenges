fbad72bb9619ab351241b25234048341b9d39831
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-06-26 23:42:27 -0500

    Create the descending order challenge

---
+++ python/descending_order.py
@@ -0,0 +1,31 @@
+"""
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+Input: 145263 Output: 654321
+Input: 123456789 Output: 987654321
+"""
+
+# Initial Thoughts:
+
+
+def descending_order(num):
+    return
+
+
+num = 42145
+print(descending_order(num))
+# Answer should be: 54421
+
+num = 145263
+print(descending_order(num))
+# Answer should be: 654321
+
+num = 123456789
+print(descending_order(num))
+# Answer should be: 987654321
+
+
+# WHAT I LEARNED:
+
