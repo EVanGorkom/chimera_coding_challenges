11153f6f9df31b551310ffc97144f26bd0c0caca
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-12-16 23:25:15 -0600

    Finish the challenge maximum_avg_subarray

--- python/leetcode75/maximum_avg_subarray.py
+++ python/leetcode75/maximum_avg_subarray.py
@@ -21,8 +21,23 @@ n == nums.length
 # Initial Thoughts:
 # For this challenge I'm definitely going to need to use the static window version of a Sliding window. I think the challenge here is going to be handling the trailing zeros. I'm not sure why they are being included here.
 
-def findMaxAverage(nums: int, k: int) -> float:
-    return
+
+def findMaxAverage(nums: list[int], k: int) -> float:
+    window_sum = 0
+    max_avg = float("-inf")
+    left = 0
+
+    for right in range(len(nums)):
+        window_sum += nums[right]
+        
+        if right - left + 1 == k:
+            window_avg = window_sum / k
+            max_avg = max(max_avg, window_avg)
+            window_sum -= nums[left]
+            left += 1
+
+    return max_avg
+
 
 
 nums = [1,12,-5,-6,50,3]
@@ -37,3 +52,7 @@ print(findMaxAverage(nums, k))
 # Answer should be: 5.00000
 
 # WHAT I LEARNED:
+# After taking some time away from coding challenges, it was nice to get back into the swing of things with this one.
+# I think this one was pretty straight forward after reviewing my notes and I enjoyed solving it. 
+# Looking around the internet I found that I'm calculating the average on the inside of the loop, but since 'k' is a fixed number, it's unnecessary to do so
+# within the loop and I can instead just solve for the max sum and calculate average at the end! Neat trick!
