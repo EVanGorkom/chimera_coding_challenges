c88d7d506d0e5479bf09c9cdd3d1448139358b14
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2025-12-20 15:53:24 -0600

    Create the unique_num_occurrance challenge

---
+++ python/leetcode75/unique_num_occurance.py
@@ -0,0 +1,28 @@
+"""
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+"""
+
+# Initial Thoughts
+
+
+
+def uniqueOccurrences(arr: list[int]) -> bool:
+    return
+
+
+
+arr = [1,2,2,1,1,3]
+print(uniqueOccurrences(arr))
+# Answer should be: True
+
+arr = [1,2]
+print(uniqueOccurrences(arr))
+# Answer should be: False
+
+arr = [-3,0,1,-3,1,1,1,-3,10,0]
+print(uniqueOccurrences(arr))
+# Answer should be: True
+
+
+# WHAT I LEARNED:
+# 
